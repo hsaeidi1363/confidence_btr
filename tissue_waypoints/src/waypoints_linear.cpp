b562092f18dfad0428f227f1532d7c01343767d3
Hamed Saeidi <hamed.saeidi.rme@gmail.com>
2018-11-08 10:15:42 -0500

    added and tested the linear interpolation code for the new test conditions

--- tissue_waypoints/src/waypoints_linear.cpp
+++ tissue_waypoints/src/waypoints_linear.cpp
@@ -49,21 +49,18 @@ void filter_waypoints(pcl::PointCloud<pcl::PointXYZI> & _raw, pcl::PointCloud<pc
 }
 
 int main(int argc, char **argv){
-	ros::init(argc, argv, "waypoints_client");
+	ros::init(argc, argv, "waypoints_linear");
 	ros::NodeHandle n;
 	ros::Rate loop_rate(2);
 	ros::Subscriber pcl_sub = n.subscribe("/nir_overlay_intel/cog",1,get_pcl);
 	ros::Subscriber polygon_sub = n.subscribe("/nir_overlay_intel/polygon3D_cog",1, get_polygon);
-	ros::Publisher traj_pub = n.advertise<pcl::PointCloud<pcl::PointXYZI> > ("tissue_traj",1);
 	ros::Publisher short_traj_pub = n.advertise<pcl::PointCloud<pcl::PointXYZI> > ("short_tissue_traj",1);
 	ros::Publisher filtered_traj_pub = n.advertise<pcl::PointCloud<pcl::PointXYZI> > ("filtered_tissue_traj",1);
-	ros::ServiceClient wp_client = n.serviceClient<tissue_waypoints::Trajectory>("FindTrajectory");
+
 	
-	ROS_INFO("service available");
 	int seq = 0;
  	while(ros::ok()){
-
-		pcl::PointCloud<pcl::PointXYZI> output_traj;			
+	
 		pcl::PointCloud<pcl::PointXYZI> short_output_traj;		
 
 		if(marker_cog_pcl.points.size() == 0 || marker_cog_pcl.points.size() == 1){
@@ -71,52 +68,33 @@ int main(int argc, char **argv){
 		}else{
 			int pts_len = marker_cog_pcl.points.size();
 			for (int pts_id =0; pts_id < pts_len; pts_id++){
-				tissue_waypoints::Trajectory srv;
 				int start_pt = pts_id % pts_len;
 				int end_pt = (pts_id +1 )% pts_len;
-				/*srv.request.start.push_back(marker_cog_pcl.points[start_pt].x); 
-				srv.request.start.push_back(marker_cog_pcl.points[start_pt].y); 
-				srv.request.start.push_back(marker_cog_pcl.points[start_pt].z); 
-				srv.request.end.push_back(marker_cog_pcl.points[end_pt].x); 
-				srv.request.end.push_back(marker_cog_pcl.points[end_pt].y); 
-				srv.request.end.push_back(marker_cog_pcl.points[end_pt].z); 
-				*/
-				srv.request.start.push_back(markers3D.points[start_pt].x); 
-				srv.request.start.push_back(markers3D.points[start_pt].y); 
-				srv.request.start.push_back(markers3D.points[start_pt].z); 
-				srv.request.end.push_back(markers3D.points[end_pt].x); 
-				srv.request.end.push_back(markers3D.points[end_pt].y); 
-				srv.request.end.push_back(markers3D.points[end_pt].z); 				
-				if(wp_client.call(srv)){
-					ROS_INFO("got the points back:");
+				pcl::PointXYZI start_point;
+				pcl::PointXYZI end_point;
+				pcl::PointCloud<pcl::PointXYZI> pt_list;
 
-					std::cout << srv.response.path_x.size()<<std::endl;
-					pcl::PointCloud<pcl::PointXYZI> pt_list;
-					for(int i = 0 ; i< srv.response.path_x.size(); i++){
-						pcl::PointXYZI xyzi;
-						xyzi.x = srv.response.path_x[i];	
-						xyzi.y = srv.response.path_y[i];	
-						xyzi.z = srv.response.path_z[i];
-						xyzi.intensity = 100;	
-						output_traj.points.push_back(xyzi);
-						pt_list.points.push_back(xyzi);
-					}
-					pt_list.points.push_back(marker_cog_pcl.points[end_pt]);
-					detect_key_points(pt_list,short_output_traj);
-					std_msgs::Header header;
-					header.stamp = ros::Time::now();
-					header.seq = seq++; // is this correct?
-					header.frame_id = std::string("camera_color_optical_frame");
-					output_traj.header = pcl_conversions::toPCL(header);
-					short_output_traj.header = pcl_conversions::toPCL(header);
-					traj_pub.publish(output_traj);
-					short_traj_pub.publish(short_output_traj);
-				}
-				else
-				{
-					ROS_INFO("Failed to call service trajectory");
-					return 1;
-				}
+				start_point.x = markers3D.points[start_pt].x; 
+				start_point.y = markers3D.points[start_pt].y; 
+				start_point.z = markers3D.points[start_pt].z; 
+				start_point.intensity = 100;
+				
+				pt_list.points.push_back(start_point);
+
+				end_point.x = markers3D.points[end_pt].x; 
+				end_point.y = markers3D.points[end_pt].y; 
+				end_point.z = markers3D.points[end_pt].z; 
+				end_point.intensity = 100;
+
+				pt_list.points.push_back(end_point);
+				detect_key_points(pt_list,short_output_traj);
+				std_msgs::Header header;
+				header.stamp = ros::Time::now();
+				header.seq = seq++; // is this correct?
+				header.frame_id = std::string("camera_color_optical_frame");
+				short_output_traj.header = pcl_conversions::toPCL(header);
+				short_traj_pub.publish(short_output_traj);
+				
 			}
 	      		filter_waypoints(short_output_traj,filtered_output_traj);
 			ROS_INFO("Filtered one loop of traj");
@@ -141,23 +119,40 @@ float dist_3d(pcl::PointXYZI & pt1, pcl::PointXYZI & pt2){
 void detect_key_points(pcl::PointCloud<pcl::PointXYZI> &in_list, pcl::PointCloud<pcl::PointXYZI> & out_traj){
 	float traj_length = 0.0;
 	int mid_points = 4;
-	std::vector<float> points_loc;// the location of point on the path based on the traveled distance between the start and end point
-	points_loc.push_back(0.0); //distance of the start point on the path is zero
-	for (int i = 1; i < in_list.points.size(); i++){
-		traj_length += dist_3d(in_list.points[i], in_list.points[i-1]);
-		points_loc.push_back(traj_length);
-	} 
-	float traj_seg_len = (float)traj_length/ (mid_points+1);
+	
+	pcl::PointXYZI p1 = in_list[0];
+	pcl::PointXYZI p2 = in_list[1];
+        float x1 = p1.x;
+	float x2 = p2.x;
+      
+	float y1 = p1.y;
+	float y2 = p2.y;
+
+	float z1 = p1.z;
+	float z2 = p2.z;
+
+        float dx=(x2-x1)/(mid_points+1);
+        float dy=(y2-y1)/(mid_points+1);
+        float dz=(z2-z1)/(mid_points+1);
+
+
 	for (int j = 1; j<= (mid_points + 1); j++){
-		float min_dist = 1000.0;
-		int min_ind = 0;
-		float mid_point_loc = traj_seg_len*j; // e.g. for mid_points = 4 (4 waypoints between the start and end) we will have 5 path segments and mid_point_loc = 20%, 40%, 60%, and 80% of the path
-		for (int i = 0; i < in_list.points.size(); i++){
-			if (fabs(mid_point_loc - points_loc[i]) < min_dist){
-				min_dist = fabs(mid_point_loc - points_loc[i]);
-				min_ind = i;
-			}
-		}
-		out_traj.points.push_back(in_list.points[min_ind]);
+		 // e.g. for mid_points = 4 (4 waypoints between the start and end) we will have 5 path segments and mid_point_loc = 20%, 40%, 60%, and 80% of the path
+		pcl::PointXYZI pi;
+		pi.x = x1 + dx*(j);
+		pi.y = y1 + dy*(j);
+		pi.z = z1 + dz*(j);
+		pi.intensity = 100;
+		out_traj.points.push_back(pi);
 	}
 }
+
+
+      
+     
+
+
+	
+
+   
+	
